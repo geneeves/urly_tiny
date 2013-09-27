e27d389075e5827b75033a4a9bcb10a2a5b3a787
epicodus <student@epicodus.com>
2013-09-27 09:30:19 -0700

    Add README and setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlyTiny::Application.config.secret_key_base = 'a7ab0dd1969e2023761da1458c1023f8d5c5dd9050586ddd2cc697bcb7d0034e99bc2c02822ca2ae84dd8c24c68d55c8a6b1fbe70024c428b6e6d1e4d1e11d69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UrlyTiny::Application.config.session_store :cookie_store, key: '_urly_tiny_session'
