4ed93f356229ab3fc4e890cf7c874c712c6d011f
Brittany jazzy Winston & Geneva Skeen <geneva.skeen@gmail.com>
2013-09-27 17:42:54 -0700

    Finished visit and url models and basic views. WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130927163422) do
+ActiveRecord::Schema.define(version: 20130927225534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "shortened_urls", force: true do |t|
+  create_table "urls", force: true do |t|
     t.string   "original_url"
     t.string   "shortened_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "visits", force: true do |t|
+    t.integer  "url_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
